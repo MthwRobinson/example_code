3a7463ed9ad93713be2a6706f06011ab75f42843
mrobinson72 <mrobinson72@gatech.edu>
2016-05-23 22:54:13 -0400

    more examples

--- knn.py
+++ knn.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame
 import statsmodels.formula.api as sm
+import statsmodels.api as sma
 
 import sklearn as sk 
 from sklearn import linear_model
@@ -41,20 +42,46 @@ df = DataFrame({'x':x,'y':y,'w':w})
 
 ## Linear regression
 lm = linear_model.LinearRegression()
+data = np.matrix(df)
 x,y = data[:,1], data[:,2]
 lm.fit(x,y)
 
 lm = sm.ols(formula = 'y ~ x', data = df).fit()
 print lm.summary()
-lm.predict()
 exog = pd.DataFrame({'x':[10,15]})
+lm.predict(exog)
 lm.resid
 lm.params
 
 # Weighted Least Squares
 nsamp = df.shape[0]
-y = np.array(df['y'])
+Y = np.array(df['y'])
 X = np.c_[ np.ones(nsamp), np.array(df['x'])]
-fm1 = sm.WLS(y, X, weights = 1 / w**2)
+fm1 = sm.WLS(Y, X, weights = 1 / w**2)
 res_fm1 = fm1.fit()
 res_fm1.summary()
+
+# Plots
+plt.figure()
+plt.scatter(x,y)
+x_range = arange(0,20,.1)
+exog = DataFrame({'x':x_range})
+y_pred = lm.predict(exog)
+plt.plot(x_range,y_pred)
+plt.close()
+
+res = lm.resid
+fig = sma.qqplot(resid)
+plt.show()
+
+# KNN
+
+file = "http://www2.isye.gatech.edu/~ymei/7406/Handouts/mixtureexample.csv"
+df = pd.read_csv(file, header = True)
+rows = [0,1,2,197,198,199]
+df.ix[rows]
+
+data = np.matrix(df)
+x1 = data[:,0]
+x2 = data[:,1]
+y = data[:,2]
